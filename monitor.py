9b11d0a01d7e92c48a53b8db847c48107cc60bc5
J. Chitty <chitty@eosargentina.io>
2025-01-27 20:06:41 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,12 +2,15 @@ import requests as r
 import json
 import time
 
+current_time = time.localtime()
+
 while True:
     try:
+        formatted_time = time.strftime("%d-%m-%Y %H:%M:%S", current_time)
+        print(formatted_time)
         data = r.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD').json()
         print('BTC', data["BTC"]['USD'])
         print('ETH', data["ETH"]['USD'])
-        print('...........1min..........')
         time.sleep(60)
     except:
         print('Internet or API Error')
