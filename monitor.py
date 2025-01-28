26f8a3633b51b2a590c0f21e4d7966368c273f5a
J. Chitty <chitty@eosargentina.io>
2025-01-28 12:58:18 +0100

    Update monitor.py
    duh

--- monitor.py
+++ monitor.py
@@ -2,10 +2,9 @@ import requests as r
 import json
 import time
 
-current_time = time.localtime()
-
 while True:
     try:
+        current_time = time.localtime()
         formatted_time = time.strftime("%d-%m-%Y %H:%M:%S", current_time)
         print(formatted_time)
         data = r.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD').json()
